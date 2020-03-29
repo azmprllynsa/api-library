f1f250f5e89dcd4dd88dfe7b847ae20304881429
azmprllynsa <azmi.naisa@gmail.com>
2020-03-30 03:06:48 +0700

    fix bugs by eslint

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
 const book = require('./book');
 const user = require('./user');
-const loan = require('./loan')
+const loan = require('./loan');
+
 const router = express.Router();
 
 router
-    .use('/book', book)
-    .use('/user', user)
-    .use('/loan', loan)
+  .use('/book', book)
+  .use('/user', user)
+  .use('/loan', loan);
 
 
 module.exports = router;
